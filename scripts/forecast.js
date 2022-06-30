13f7b7455a4a4b2780ee5f570228b454f9930748
Paweł Cyrzyk <95565770+pabloscyzoryk@users.noreply.github.com>
2022-06-30 21:49:05 +0200

    Add files via upload

---
+++ scripts/forecast.js
@@ -0,0 +1,24 @@
+class Forecast {
+  constructor(){
+    this.key = 'EBxP3vQ0rfF55S6Xpgf9JuSDPIA0DsJ3';
+    this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  }
+  async updateCity(city){
+    const cityDets = await this.getCity(city);
+    const weather = await this.getWeather(cityDets.Key);
+    return { cityDets, weather };
+  }
+  async getWeather(id){
+    const query = `${id}?apikey=${this.key}`;
+    const response = await fetch(this.weatherURI + query);
+    const data = await response.json();
+    return data[0];
+  }
+  async getCity(city){
+    const query = `?apikey=${this.key}&q=${city}`;
+    const response = await fetch(this.cityURI + query);
+    const data = await response.json();
+    return data[0];
+  }
+}
